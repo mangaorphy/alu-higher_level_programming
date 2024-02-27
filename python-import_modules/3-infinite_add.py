7dad55d0be3afa55ddd20bbbb8bd7b3e977e1e61
mangaorphy <o.manga@alustudent.com>
2024-02-26 18:34:52 -0800

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     addint = 0
     for i in range(1, len(argv)):
-        addint += int(arg[i])
+        addint += int(argv[i])
     print("{}".format(addint))
