1efae2ef2fda460b9d4a931a7f45fdeaa9f3e36d
mangaorphy <o.manga@alustudent.com>
2024-02-26 18:55:49 -0800

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     allfun = dir()
     for i in range(0, len(allfun)):
-        if allfun[i][:2] != "_":
+        if allfun[i][:2] != "__":
             print("{:s}".format(allfun[i]))
