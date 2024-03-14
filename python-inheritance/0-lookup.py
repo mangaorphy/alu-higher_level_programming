eb13416303fddd521bbdb348155abff6c5e8e6cf
mangaorphy <o.manga@alustudent.com>
2024-03-14 08:56:39 -0700

    0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+''' A function that returns the lists of variable attributes and methods of an object.'''
+
+
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
