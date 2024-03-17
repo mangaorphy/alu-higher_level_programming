d4bdfe71873169125421ca33def84ae1980f6d37
mangaorphy <o.manga@alustudent.com>
2024-03-17 05:53:32 -0700

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the number of lines
-    of a text file
+""" Module that contains a function that returns the number of characters of a text file
 """
 
 
-def number_of_lines(filename=""):
-    """ Function that reads from a file and prints its number of lines
-
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
-    """
-    n_lines = 0
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            n_lines += 1
-    return n_lines
+def write_file(filename="", text=""):
+    """Writes string text file (UTF8) returns number text written."""
+    with open(filename, encoding='utf-8') as f:
+        f.write(text)
+        return len(text)
