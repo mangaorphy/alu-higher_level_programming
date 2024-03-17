31b785807706abc9107d20e16f32093f44b93d98
mangaorphy <o.manga@alustudent.com>
2024-03-17 05:57:44 -0700

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the number of characters of a text file
+""" Module that contains a function that writes to a text file
 """
 
 
 def write_file(filename="", text=""):
-    """Writes string text file (UTF8) returns number text written."""
-    with open(filename, encoding='utf-8') as f:
-        f.write(text)
-        return len(text)
+    """ Function that writes to a text file
+
+    Args:
+        filename: filename
+        text: text to write
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
