457418d06a4a56a53664b86d536e13bf075b88fb
mangaorphy <o.manga@alustudent.com>
2024-05-27 21:49:20 +0200

    Alll files added recently.

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const number = parseInt(process.argv[2], 10);
 
-if (number.isNaN(number)) {
+if (Number.isNaN(number)) {
     console.log('Not a number');
 }else {
     console.log('My number: ' + number);
