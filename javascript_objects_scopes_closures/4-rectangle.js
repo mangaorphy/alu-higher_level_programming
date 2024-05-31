44917534d1dafa536d2f7510d8956ebd48df116b
mangaorphy <o.manga@alustudent.com>
2024-05-31 14:40:52 +0200

    Added 4-rectangle.js.

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 class Rectangle {
     constructor (w, h) {
         if ((w > 0) && (h > 0)) {
@@ -9,22 +8,21 @@ class Rectangle {
     }
 
     print () {
-        for (let i = 0; i < this.width; i++){
-            for (let i = 0; i < this.width; i++) {
-                let b = '';
-                for (let j = 0; j < this.height; j++){
-                    let b = 'X';
-                }
-                console.log(b);
+        for (let i = 0; i < this.height; i++) {
+            let s = '';
+            for (let j = 0; j < this.width; j++) {
+                s += 'X';
             }
+            console.log(s);
         }
     }
+
     rotate () {
         const exchange = this.width;
         this.width = this.height;
         this.height = exchange;
-
     }
+
     double () {
         this.width *= 2;
         this.height *= 2;
