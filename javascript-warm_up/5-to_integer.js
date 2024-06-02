c2ace9d33ba1232b9af2409914496cda8c024445
mangaorphy <o.manga@alustudent.com>
2024-06-02 04:28:52 -0700

    semistandard fixing

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,8 +2,7 @@
 const number = parseInt(process.argv[2], 10);
 
 if (Number.isNaN(number)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + number);
+  console.log('My number: ' + number);
 }
-
--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,11 +2,10 @@
 
 let i = parseInt(process.argv[2], 10);
 if (Number.isNaN(i)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-    while (i > 0) {
-        console.log('C is fun');
-        i--;
-    }
+  while (i > 0) {
+    console.log('C is fun');
+    i--;
+  }
 }
-
