d2bd33afb2789ee8fdde9717a78ce948a5f35ca6
mangaorphy <o.manga@alustudent.com>
2024-06-04 22:13:47 +0200

    Added 3-starwars_title.js

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieID = process.argv[2];
+const apiURL = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request(apiURL, (error, response, body) => {
+  if (error == null) {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
