7a90b43b499f0259b2c1b2cfb04602058a167ddb
mangaorphy <o.manga@alustudent.com>
2024-06-04 17:43:56 +0200

    Added 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
