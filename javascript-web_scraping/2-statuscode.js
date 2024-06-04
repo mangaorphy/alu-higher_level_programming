994624cafdc74b0e4a3590d862ea32a3d47e2dfb
mangaorphy <o.manga@alustudent.com>
2024-06-04 17:43:56 +0200

    Added 2-statuscode.js

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.log('An error occurred: ', error);
+  } else {
+    console.log('Code: ', response.statusCode);
+  }
+});
